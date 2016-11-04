f5ba9f601df332030dfb4fd3ceb000f745b4f17c
ukhuranauva <rubyurja@msn.com>
2016-11-04 13:14:43 +0100

    everything works

--- app/src/main/java/com/example/urja/urjakhurana_pset1/MainActivity.java
+++ app/src/main/java/com/example/urja/urjakhurana_pset1/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,29 +14,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void drawPart(View view) {
+        // initialize img to random image to prevent error
         ImageView img = (ImageView) findViewById(R.id.armsImg);
-        if (view == findViewById(R.id.armsBox)) {
-            img =(ImageView) findViewById(R.id.armsImg);
-        } else if (view == findViewById(R.id.earsBox)) {
-            img =(ImageView) findViewById(R.id.earsImg);
-        } else if (view == findViewById(R.id.eyebrowsBox)) {
-            img =(ImageView) findViewById(R.id.eyebrowsImg);
-        } else if (view == findViewById(R.id.eyesBox)) {
-            img =(ImageView) findViewById(R.id.eyesImg);
-        } else if (view == findViewById(R.id.glassesBox)) {
-            img =(ImageView) findViewById(R.id.glassesImg);
-        } else if (view == findViewById(R.id.hatBox)) {
-            img =(ImageView) findViewById(R.id.hatImg);
-        } else if (view == findViewById(R.id.mouthBox)) {
-            img =(ImageView) findViewById(R.id.mouthImg);
-        } else if (view == findViewById(R.id.mustacheBox)) {
-            img =(ImageView) findViewById(R.id.mustacheImg);
-        } else if (view == findViewById(R.id.noseBox)) {
-            img =(ImageView) findViewById(R.id.noseImg);
-        } else if (view == findViewById(R.id.shoesBox)) {
-            img =(ImageView) findViewById(R.id.shoesImg);
+        // decide which checkbox is clicked and get corresponding image of that part
+        switch(view.getId()) {
+            case R.id.armsBox:
+                img = (ImageView) findViewById(R.id.armsImg);
+                break;
+            case R.id.earsBox:
+                img = (ImageView) findViewById(R.id.earsImg);
+                break;
+            case R.id.eyebrowsBox:
+                img = (ImageView) findViewById(R.id.eyebrowsImg);
+                break;
+            case R.id.eyesBox:
+                img = (ImageView) findViewById(R.id.eyesImg);
+                break;
+            case R.id.glassesBox:
+                img = (ImageView) findViewById(R.id.glassesImg);
+                break;
+            case R.id.hatBox:
+                img = (ImageView) findViewById(R.id.hatImg);
+                break;
+            case R.id.mouthBox:
+                img =(ImageView) findViewById(R.id.mouthImg);
+                break;
+            case R.id.mustacheBox:
+                img =(ImageView) findViewById(R.id.mustacheImg);
+                break;
+            case R.id.noseBox:
+                img =(ImageView) findViewById(R.id.noseImg);
+                break;
+            case R.id.shoesBox:
+                img =(ImageView) findViewById(R.id.shoesImg);
+                break;
         }
-        if (img.getVisibility() == View.INVISIBLE) {
+
+        // if checkbox is checked or not
+        boolean checked = ((CheckBox) view).isChecked();
+        // only show image if checkbox is checked and image is invisible
+        if (img.getVisibility() == View.INVISIBLE && checked) {
             img.setVisibility(View.VISIBLE);
         }else{
             img.setVisibility(View.INVISIBLE);
